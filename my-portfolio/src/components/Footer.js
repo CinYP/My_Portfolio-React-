6036d8f7afae6ee5dc2f0d13587316b0199973d4
CinYP <111353883+CinYP@users.noreply.github.com>
2023-02-08 10:28:17 -0500

    rough footer put up and styled

--- my-portfolio/src/components/Footer.js
+++ my-portfolio/src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link }from 'react-router-dom';
+//import { Link }from 'react-router-dom';
 import "../styles/Footer.css";
 
 
@@ -7,7 +7,9 @@ export default function Footer() {
   return (
     <div className='footer'>
       <div className='footerlinks'>
-      
+      <div className='icons'>
+        coming soon....
+      </div>
       </div>
       <p> &copy; 2023 Created by CinthiaPruitt.com </p>
     </div>
