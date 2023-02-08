07adcac184ee83ff167222d57b0fba9bd7fc421c
CinYP <111353883+CinYP@users.noreply.github.com>
2023-02-08 11:07:11 -0500

    styling home page about me section, skills, and pops of color

--- my-portfolio/src/components/Footer.js
+++ my-portfolio/src/components/Footer.js
@@ -7,11 +7,11 @@ export default function Footer() {
   return (
     <div className='footer'>
       <div className='footerlinks'>
-      <div className='icons'>
-        coming soon....
+      <div className='footericons'>
+        Coming Soon....
       </div>
       </div>
-      <p> &copy; 2023 Created by CinthiaPruitt.com </p>
+      <p> &copy; 2023 Created by Cinthia Pruitt </p>
     </div>
   )
 }
