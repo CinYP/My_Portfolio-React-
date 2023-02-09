9d73c650024ca1d2528f9d4fa5962a6ff27f2489
CinYP <111353883+CinYP@users.noreply.github.com>
2023-02-08 20:40:46 -0500

    preping to create project page

--- my-portfolio/src/pages/Portfolio.js
+++ my-portfolio/src/pages/Portfolio.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import "../styles/ProjectTile.css"
 
 export default function Portfolio() {
   return (
-    <div>
-      Portfolio
+    <div className='projects'>
+      <h1> My Personal Projects </h1>
+      <div className='projectList'>
+
+      </div>
     </div>
   )
 }
